60c4a93e33f51271bc6a92b4a2f2763b2e0497b4
Atharv Sharma <atharvsharma1234567890@gmail.com>
2025-01-13 23:47:52 +0530

    day 10

--- Walmart/main.py
+++ Walmart/main.py
@@ -53,3 +53,74 @@ class Solution:
             l[i].add(nums[i])
             if len(l[i]) > len(l[res]) : res = i
         return list(l[res])
+    
+#K-diff pairs in an array
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        if k == 0 :
+            nums = Counter(nums)
+            res = 0
+            for i in nums :
+                if nums[i] > 1 : 
+                    res += 1
+            return res
+        nums = set(nums)
+        res = 0
+        cnums = nums.copy()
+        for i in nums : 
+            if i+k in cnums :
+                # print(i, i+k)
+                res += 1
+            if i-k in  cnums : 
+                # print(i, i+k)
+                res += 1
+            cnums.remove(i)
+        # print(res)
+        return res
+    
+#Rotate function
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)) :
+            res += nums[i]*i
+        curr = res
+        s = sum(nums)
+        for i in range(len(nums)-1, 0, -1) :
+            # nums = nums[-1:] + nums[:-1]
+            curr = curr + s - nums[i]*len(nums)
+            if curr > res : res = curr
+            # res = max(res, curr)
+        return res
+
+#Get Equal Substring Within Budget
+class Solution :
+    def equalSubstring(self, s, t, cost):
+        i = 0
+        for j in range(len(s)):
+            cost -= abs(ord(s[j]) - ord(t[j]))
+            if cost < 0:
+                cost += abs(ord(s[i]) - ord(t[i]))
+                i += 1
+        return j - i + 1
+
+#Word break
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool :
+        ld = len(wordDict)
+        fallacies = []
+        def wb(s) -> bool :
+            ls = len(s)
+            if ls == 0 :
+                return True
+            elif s in fallacies :
+                return False
+            # print(wordDict)
+            for ss in wordDict :
+                lss = len(ss)
+                if ss == s[:lss] :
+                    if wb(s[lss:]) == True :
+                        return True
+            fallacies.append(s)
+            return False
+        return wb(s)
