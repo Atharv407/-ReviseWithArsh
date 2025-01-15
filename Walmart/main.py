afbafe011d0b1bcdbbc8bb055c978572aec5c200
Atharv Sharma <atharvsharma1234567890@gmail.com>
2025-01-15 22:59:02 +0530

    day 12

--- Walmart/main.py
+++ Walmart/main.py
@@ -156,3 +156,90 @@ class Solution:
             return not (y <= 0.5 * x + 7 or y > x or y > 100 and x < 100)
         c = Counter(ages)
         return sum(request(a, b) * c[a] * (c[b] - (a == b)) for a in c for b in c)
+
+#Maximum length of Repeated Subarray
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        res = 0
+        dp = [[0]*len(nums2) for i in nums1]
+        for i in range(len(nums1)) :
+            for j in range(len(nums2)) :
+                if nums1[i] == nums2[j]: 
+                    if i-1 > -1 and j - 1 > -1 : 
+                        dp[i][j] = dp[i-1][j-1]
+                    dp[i][j] += 1
+                    if dp[i][j] > res :
+                        res = dp[i][j]
+        # print(dp)
+        return res
+    
+#Battleships in a Board
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        res = 0
+        v = [0]*len(board[0])
+        # vr = [0]*len(board)
+        h = 0
+        for i in range(len(board)) :
+            h = 0
+            print(res, v)
+            for j in range(len(board[0])) :
+                if board[i][j] == 'X' :
+                    if h == 1 :
+                        v[j-1] = 0
+                    elif h == 0 :
+                        v[j] += 1
+                    h += 1
+                else : 
+                    if h > 1 : 
+                        res += 1
+                    h = 0
+                    if v[j] > 0 : 
+                        res += 1
+                        v[j] = 0
+            if h > 1 : 
+                res += 1
+                v[-1] = 0
+        print(res, h, v)
+        
+        # else :
+        for j in v : 
+            if j > 0 : res += 1
+
+        return res 
+                    
+            
+#Verify Preorder Serialization of a Binary Tree
+class Solution:
+    def isValidSerialization(self, p: str) -> bool:
+        def chk(i, j) :
+            if p[i] == '#' : return i + 1
+            if j==i : return j
+            if j-i < 3 : return i
+
+            le = chk(i+1, j)
+            i = le
+
+            re = chk(i, j)
+
+            return re
+            
+        p = p.split(',')
+        if chk(0, len(p)) != len(p) :
+            return False
+        return True
+
+
+#Count the Number of Square-Free Subsets
+class Solution:
+    def squareFreeSubsets(self, nums):
+        chk = {1:0, 2:1, 3:2, 5:4, 6:3, 7:8, 10:5, 11:16, 13:32, 14:9, 15:6, 17:64, 19:128, 21:10, 22:17, 23:256, 26:33, 29:512, 30:7}
+        count = defaultdict(int)
+        for n in nums:
+            if n in chk:
+                for k in count.copy():
+                    if chk[n] & k == 0:
+                        count[chk[n]|k] += count[k]
+                count[chk[n]] += 1
+        return sum(count.values()) % (10 ** 9 + 7)
+        
